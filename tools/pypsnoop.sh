3638849e94bdb037f0a405375686a8550e6df904
JavidDitty <javidditty@gmail.com>
2026-01-28 16:15:25 -0500

    Add nonblocking option to pypsnoop

--- tools/pypsnoop.sh
+++ tools/pypsnoop.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
 set -u
-"$CINTENT_PYSPY" record \
---pid "$1" \
---full-filenames \
---output "$CINTENT_LOGS/$(date +%s%N).$CINTENT_STEP_ID.speedscope.json" \
---format speedscope \
---duration unlimited \
---rate 100 \
---subprocesses \
---function \
-> /dev/null &
+if [ "$CINTENT_NONBLOCKING" == "true" ]; then
+    "$CINTENT_PYSPY" record \
+    --pid "$1" \
+    --full-filenames \
+    --output "$CINTENT_LOGS/$(date +%s%N).$CINTENT_STEP_ID.speedscope.json" \
+    --format speedscope \
+    --duration unlimited \
+    --rate 100 \
+    --subprocesses \
+    --function \
+    --nonblocking \
+    > /dev/null &
+else
+    "$CINTENT_PYSPY" record \
+    --pid "$1" \
+    --full-filenames \
+    --output "$CINTENT_LOGS/$(date +%s%N).$CINTENT_STEP_ID.speedscope.json" \
+    --format speedscope \
+    --duration unlimited \
+    --rate 100 \
+    --subprocesses \
+    --function \
+    > /dev/null &
+fi
